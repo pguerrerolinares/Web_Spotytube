f29ee6ed6df1b352d613ef2e92ff428449b00ec3
Paul <pguerrero002@ikasle.ehu.eus>
2019-04-25 17:12:51 +0200

    fix show tracks

--- Spotytube/main.py
+++ Spotytube/main.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # Copyright 2016 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -39,6 +40,10 @@ requests_toolbelt.adapters.appengine.monkeypatch()
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
+
+# Google keys
+google_secret_key = "1FfMRgteyw6T8b46872U0dgb"
+client_id = "990115409802-q9o1n9f5hab5lrlg84l21u2si23m90ph.apps.googleusercontent.com"
 
 
 def is_spotify_token_expired(token_info):
@@ -82,6 +87,19 @@ def _get_access_token():
     return token_info
 
 
+@app.route('/')
+def index():
+    spotify_token = session.get('spotify_token')
+    if spotify_token is None:
+        # Si no hay token
+        return login_spotify()
+    else:
+        if is_spotify_token_expired(spotify_token):
+            return login_spotify()
+
+    return render_template('header.html', template_selector=0)
+
+
 @app.route('/LoginSpotify')
 def login_spotify():
     logging.debug('ENTERING LoginAndAuthorizeHandler --->')
@@ -94,17 +112,21 @@ def login_spotify():
     return index()
 
 
-@app.route('/')
-def index():
-    spotify_token = session.get('spotify_token')
-    if spotify_token is None:
-        # Si no hay token
-        return login_spotify()
-    else:
-        if is_spotify_token_expired(spotify_token):
-            return login_spotify()
+@app.route('/SearchSpotify', methods=['GET'])
+def search_spotify():
+    spotify_token = session['spotify_token']['access_token']
+    playlist2search = request.args.get("id")
+    type_seach = request.args.get('typesearch')
 
-    return render_template('header.html', template_selector=0)
+    if playlist2search is None:
+        if type_seach is not "name":
+            to_search = request.args.get('search')
+            list_playlist = search_playlists(spotify_token, to_search)
+            _print_playlists(list_playlist)
+    template_playlist_names = session.get('playlist_names')
+
+    return render_template('header.html', playlist_names=template_playlist_names,
+                           template_selector=0)
 
 
 def _print_playlists(list_playlist):
@@ -127,7 +149,8 @@ def show_tracks():
     spotify_token = session['spotify_token']['access_token']
     playlist2search = request.args.get("id")
 
-    tracks = search_playlists(spotify_token, playlist2search)
+    tracks = get_tracks_from_playlist(spotify_token, playlist2search)
+    print(tracks)
     _print_tracks(tracks)
     template_tracks = session.get('tracks_names')
 
@@ -135,6 +158,17 @@ def show_tracks():
 
 
 def _print_tracks(tracks):
+    """
+    Posiciones de la array:
+    0 -> nombre canción
+    1 -> nombre artista
+    2 -> nombres de artistas
+    3 -> duración(ms)
+    4 -> url imagen
+    5 -> url preview song
+    :param playlist_tracks: Lista de canciones de la playlist
+    :return:
+    """
     session['tracks_names'] = []
     for track in tracks:
         current_track = track['track']
@@ -161,21 +195,55 @@ def _print_tracks(tracks):
         session['tracks_names'].append(array)
 
 
-@app.route('/SearchSpotify', methods=['GET'])
-def search_spotify():
-    spotify_token = session['spotify_token']['access_token']
-    playlist2search = request.args.get("id")
-    type_seach = request.args.get('typesearch')
+@app.route('/LoginGoogle')
+def login_google():
+    # Enviar una solicitud de autenticacion a google
+    redirect_uri = 'http://localhost:8080/oauth2callback'  # Localhost
+    # redirect_uri = 'http://spotytube.appspot.com/oauth2callback'
 
-    if playlist2search is None:
-        if type_seach is not "name":
-            to_search = request.args.get('search')
-            list_playlist = search_playlists(spotify_token, to_search)
-            _print_playlists(list_playlist)
-    template_playlist_names = session.get('playlist_names')
+    server = 'https://accounts.google.com/o/oauth2/v2/auth'
 
-    return render_template('header.html', playlist_names=template_playlist_names,
-                           template_selector=0)
+    params = {'client_id': client_id,
+              'response_type': 'code',
+              'scope': 'https://www.googleapis.com/auth/youtube',
+              'redirect_uri': redirect_uri,
+              'access_type': 'offline'}
+
+    headers = {'User-Agent': 'Google App Engine'}
+
+    params_encoded = urllib.urlencode(params)
+
+    response = requests.get(server, headers=headers, params=params_encoded)
+    if response.status_code == 200:
+        return redirect(str(response.url))
+
+
+@app.route('/oauth2callback')
+def oauthcallback_google():
+    # Get code
+    code = request.args.get('code')
+
+    # Get token
+    redirect_uri = 'http://localhost:8080/oauth2callback'  # Localhost
+
+    headers = {
+        'Host': 'www.googleapis.com',
+        'Content-Type': 'application/x-www-form-urlencoded'}
+    data = {
+        'code': code,
+        'redirect_uri': redirect_uri,
+        'client_id': client_id,
+        'client_secret': google_secret_key,
+        'grant_type': 'authorization_code'
+    }
+    response = requests.post("https://www.googleapis.com/oauth2/v4/token", headers=headers, data=data)
+
+    json_respuesta = json.loads(response.content)
+    # print json_respuesta
+    access_token = json_respuesta['access_token']
+
+    session['yt_token'] = access_token
+    redirect('/')
 
 
 @app.errorhandler(500)
